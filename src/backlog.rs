c81fab74509d3e0df381781120b206a54378d0ca
Quill Young <redtypesw@gmail.com>
2025-08-14 14:11:05 -0400

    Some work

--- src/backlog.rs
+++ src/backlog.rs
@@ -1,9 +1,10 @@
 use crate::state::AppState;
 
 pub async fn fetch_backlog(
+  state: &AppState,
   user_id: impl AsRef<str>,
   timestamp: Option<impl AsRef<str>>,
-) -> Result<Vec<DbMessage>, sqlx::Error> {
+) -> Result<Vec<Message>, sqlx::Error> {
   let messages = if let Some(ts) = timestamp {
     sqlx::query_as("\
       SELECT * \
@@ -12,8 +13,8 @@ pub async fn fetch_backlog(
         AND createdAt > $2 \
       ORDER BY createdAt ASC;\
     ")
-      .bind(user_id)
-      .bind(ts)
+      .bind(user_id.as_ref())
+      .bind(ts.as_ref())
       .fetch_all(&state.db)
       .await?
   } else {
@@ -23,7 +24,7 @@ pub async fn fetch_backlog(
       WHERE userId = $1\
       ORDER BY createdAt ASC;\
     ")
-      .bind(user_id)
+      .bind(user_id.as_ref())
       .fetch_all(&state.db)
       .await?
   };
